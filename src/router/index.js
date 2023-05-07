5c0e41fdefdce6833ac1957cd4de20af28c7377a
Andreas George Nechita <ga.nechita@gmail.com>
2023-05-07 13:15:14 +0200

    Working on the Arbeitszeugnis page

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,12 @@ const router = createRouter({
       component: Lebenslauf
     },
     {
-      path: '/project1',
-      name: 'project1',
+      path: '/arbeitszeugnis',
+      name: 'Arbeitszeugnis',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Project1.vue')
+      component: () => import('../components/Arbeitszeugnis.vue')
     }
   ]
 })
